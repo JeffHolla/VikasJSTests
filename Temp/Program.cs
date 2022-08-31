b567129ca2cf11c27924f8872911cf2c4ee837b5
Aleksandr Goriachkin <aleksandr.goriachkin@ihsmarkit.com>
2022-09-01 00:13:23 +0400

    Added MinMax Array tests

---
+++ Temp/Program.cs
@@ -0,0 +1,34 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var nodePath = "\"C:\\Program Files\\nodejs\\node.exe\"";
+
+            string fileName = "VikasTests.js";
+            string path = @"C:\Users\Aleksandr_Goriachkin\Desktop\VikasTestsFolder\test";
+            string fileExec = $"{path}\\{fileName}";
+
+            string execCMD = $"/K node {path}\\{fileName}";
+
+         
+            var result = await Cli.Wrap(nodePath)
+                .WithArguments(args => args
+                    .Add(@$"C:\Users\Aleksandr_Goriachkin\Desktop\VikasTestsFolder\test\VikasTests.js")
+                    .Add("1 5 2"))
+                .WithValidation(CommandResultValidation.ZeroExitCode)
+                .ExecuteBufferedAsync();
+
+            var res = result.StandardOutput;
+            Console.WriteLine(res);
+        }
+    }
+}
--- VikasJSTests/UnitTest1.cs
+++ VikasJSTests/UnitTest1.cs
@@ -2,38 +2,51 @@ using NUnit.Framework;
 using System.IO;
 using System.Diagnostics;
 using System;
+using CliWrap;
+using System.Text;
+using System.Threading.Tasks;
+using CliWrap.Buffered;
 
 namespace VikasJSTests
 {
     public class Tests
     {
         private string cmdResult;
+        private Command cmd;
+
+        private string nodePath = "\"C:\\Program Files\\nodejs\\node.exe\"";
+        private string fileExec;
 
         [SetUp]
-        public void Setup()
+        public async Task Setup()
         {
             string fileName = "VikasTests.js";
             string path = @"C:\Users\Aleksandr_Goriachkin\Desktop\VikasTestsFolder\test";
-            string execCMD = $"/C node {path}/{fileName}";
-
-            Process cmd = new Process();
-            cmd.StartInfo.FileName = "cmd.exe";
-            cmd.StartInfo.Arguments = execCMD;
-            cmd.StartInfo.RedirectStandardOutput = true;
-            cmd.StartInfo.UseShellExecute = false;
-            cmd.Start();
-
-            cmd.WaitForExit();
-            
-            cmdResult = cmd.StandardOutput.ReadToEnd();
+            fileExec = $"{path}\\{fileName}";
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(arguments: new int[] { 1, 5, 2 })]
+        [TestCase(arguments: new int[] { 1, -1, 5, 2 })]
+        [TestCase(arguments: new int[] { 1, 5, 2 })]
+        public async Task MinMaxTests_Easy(int[] array)
         {
-            Assert.AreEqual("Hi", cmdResult);
-            //Assert.AreEqual("1 2 3 4 5 6 7", cmdResult);
-            //Assert.AreEqual("1 2 3 4 5 6 7", cmdResult);
+            var result = await Cli.Wrap(nodePath)
+                .WithArguments(args => args
+                    .Add(fileExec)
+                    .Add(array.GetStringArray()))
+                .ExecuteBufferedAsync();
+
+            var outPut = result.StandardOutput.Replace("\n","").Replace("\r", "").Trim();
+
+            Assert.AreEqual(array.GetStringArray(), outPut);
+        }
+    }
+
+    public static class ArrayExtensions
+    {
+        public static string GetStringArray(this int[] arr)
+        {
+            return string.Join(" ", arr);
         }
     }
 }
